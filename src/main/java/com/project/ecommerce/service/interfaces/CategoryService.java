65f6fe17b797ec441f6bdf85b67b575aa7e13832
amithkashyap <amithkashyaprv@gmail.com>
2024-12-08 18:03:28 +0530

    updated Category interface

--- src/main/java/com/project/ecommerce/service/interfaces/CategoryService.java
+++ src/main/java/com/project/ecommerce/service/interfaces/CategoryService.java
@@ -1,4 +1,10 @@
 package com.project.ecommerce.service.interfaces;
 
+import com.project.ecommerce.entity.Category;
+
+import java.util.List;
+
 public interface CategoryService {
+    List<Category> getAllCategories();
+    void createCategory(Category category);
 }

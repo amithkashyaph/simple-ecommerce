9381cb1b620b01f8d2d381ff8bb1146f39e21caf
amithkashyap <amithkashyaprv@gmail.com>
2024-12-08 18:02:58 +0530

    refactored initial APIs in the controller

--- src/main/java/com/project/ecommerce/controller/CategoryController.java
+++ src/main/java/com/project/ecommerce/controller/CategoryController.java
@@ -1,19 +1,33 @@
 package com.project.ecommerce.controller;
 
+import com.project.ecommerce.entity.Category;
+import com.project.ecommerce.service.interfaces.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CategoryController {
 
-    @GetMapping("/api/public/categories")
-    public String getAllCategories() {
-        return "Get All Categories";
+    private CategoryService categoryService;
+
+    @Autowired
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
     }
 
-    @PostMapping("/api/admin/category")
-    public String createCategory() {
+    @GetMapping("/api/public/categories")
+    public List<Category> getAllCategories() {
+        return categoryService.getAllCategories();
+    }
+
+    @PostMapping("/api/admin/categories")
+    public String createCategory(@RequestBody Category category) {
+        categoryService.createCategory(category);
         return "Creating Category";
     }
 }

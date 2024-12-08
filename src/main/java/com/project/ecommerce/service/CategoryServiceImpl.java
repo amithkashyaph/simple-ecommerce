7f1fb07e8ecbd472afd667d2656e11ca41697b72
amithkashyap <amithkashyaprv@gmail.com>
2024-12-08 18:04:37 +0530

    updated Category service impl with logic to fetch all and create categories

--- src/main/java/com/project/ecommerce/service/CategoryServiceImpl.java
+++ src/main/java/com/project/ecommerce/service/CategoryServiceImpl.java
@@ -1,4 +1,24 @@
 package com.project.ecommerce.service;
 
-public class CategoryServiceImpl {
+import com.project.ecommerce.entity.Category;
+import com.project.ecommerce.service.interfaces.CategoryService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    List<Category> categories = new ArrayList<>();
+    @Override
+    public List<Category> getAllCategories() {
+        return categories;
+    }
+
+    @Override
+    public void createCategory(Category category) {
+        category.setId(categories.size() + 1);
+        categories.add(category);
+    }
 }

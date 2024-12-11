fca3209bee05bf642474339edc925227b2c0ac88
amithkashyap <amithkashyaprv@gmail.com>
2024-12-11 23:21:58 +0530

    implemented update category service logic

--- src/main/java/com/project/ecommerce/service/CategoryServiceImpl.java
+++ src/main/java/com/project/ecommerce/service/CategoryServiceImpl.java
@@ -2,10 +2,13 @@ package com.project.ecommerce.service;
 
 import com.project.ecommerce.entity.Category;
 import com.project.ecommerce.service.interfaces.CategoryService;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
@@ -30,5 +33,16 @@ public class CategoryServiceImpl implements CategoryService {
         }
         categories.remove(category);
         return "Category with id : " + categoryId + " successfully removed!";
+    }
+
+    @Override
+    public String updateCategory(Category category, int id) {
+        Optional<Category> existingCategory = categories.stream().filter(c -> c.getId() == id).findFirst();
+        if(existingCategory.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category with id : " + id + " does not exist");
+        }
+        Category category1 = existingCategory.get();
+        category1.setName(category.getName());
+        return "Category successfully updated";
     }
 }

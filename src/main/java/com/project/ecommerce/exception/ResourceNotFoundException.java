fe6c84e477e448167fcda32da367e1fc3696baf4
amithkashyap <amithkashyaprv@gmail.com>
2025-01-08 23:21:24 +0530

    implemented resource not found exception

--- src/main/java/com/project/ecommerce/exception/ResourceNotFoundException.java
+++ src/main/java/com/project/ecommerce/exception/ResourceNotFoundException.java
@@ -1,4 +1,15 @@
 package com.project.ecommerce.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
+    private String resourceName;
+    private String field;
+    private String fieldName;
+    private int fieldId;
+
+    public ResourceNotFoundException(String resourceName, String field, int fieldId) {
+        super(String.format("%s not found with %s : %d", resourceName, field, fieldId));
+        this.resourceName = resourceName;
+        this.field = field;
+        this.fieldId = fieldId;
+    }
 }

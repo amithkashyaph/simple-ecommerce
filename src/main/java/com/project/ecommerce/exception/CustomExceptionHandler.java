e28bd89c091605ca6569b7bbe11faf1e4599fe59
amithkashyap <amithkashyaprv@gmail.com>
2025-01-10 23:36:00 +0530

    implemented custom API exception handler

--- src/main/java/com/project/ecommerce/exception/CustomExceptionHandler.java
+++ src/main/java/com/project/ecommerce/exception/CustomExceptionHandler.java
@@ -1,4 +1,22 @@
 package com.project.ecommerce.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class CustomExceptionHandler {
+
+    @ExceptionHandler(APIException.class)
+    public ResponseEntity<String> apiException(APIException e) {
+        String message = e.getMessage();
+
+        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+    }
 }

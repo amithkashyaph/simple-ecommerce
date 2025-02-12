aeee7c0025070c4ae63b17859eb860d3a9454d71
amithkashyap <amithkashyaprv@gmail.com>
2025-02-12 23:53:21 +0530

    added validation for Category model

--- src/main/java/com/project/ecommerce/entity/Category.java
+++ src/main/java/com/project/ecommerce/entity/Category.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,5 +18,6 @@ public class Category {
     private Integer id;
 
     @NotBlank
+    @Size(min = 5, message = "Category name must contain at least 5 characters")
     private String name;
 }

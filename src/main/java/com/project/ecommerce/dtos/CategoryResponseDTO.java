8fd674d28011d201a352ece6dcea6a5e10c0530a
amithkashyap <amithkashyaprv@gmail.com>
2025-07-30 19:54:04 +0530

    refactor Category response DTO

--- src/main/java/com/project/ecommerce/dtos/CategoryResponseDTO.java
+++ src/main/java/com/project/ecommerce/dtos/CategoryResponseDTO.java
@@ -4,8 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CategoryResponseDTO {
+
+    private List<CategoryRequestDTO> content;
 }

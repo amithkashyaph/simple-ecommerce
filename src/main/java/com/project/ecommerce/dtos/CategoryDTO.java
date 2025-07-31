787be900fffb642440aed0ea0198a8b25576bbe9
amithkashyap <amithkashyaprv@gmail.com>
2025-07-31 23:32:34 +0530

    renamed Category DTO

--- src/main/java/com/project/ecommerce/dtos/CategoryDTO.java
+++ src/main/java/com/project/ecommerce/dtos/CategoryDTO.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class CategoryRequestDTO {
+public class CategoryDTO {
     private Long categoryId;
     private String categoryName;
 }
--- src/main/java/com/project/ecommerce/dtos/CategoryResponseDTO.java
+++ src/main/java/com/project/ecommerce/dtos/CategoryResponseDTO.java
@@ -11,5 +11,5 @@ import java.util.List;
 @NoArgsConstructor
 public class CategoryResponseDTO {
 
-    private List<CategoryRequestDTO> content;
+    private List<CategoryDTO> content;
 }

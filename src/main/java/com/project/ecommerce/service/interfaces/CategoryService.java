9f2f37a289e31e8b67b77d2bab3f3cf5df0868c6
amithkashyap <amithkashyaprv@gmail.com>
2025-08-01 00:15:14 +0530

    updated category service interface

--- src/main/java/com/project/ecommerce/service/interfaces/CategoryService.java
+++ src/main/java/com/project/ecommerce/service/interfaces/CategoryService.java
@@ -1,11 +1,12 @@
 package com.project.ecommerce.service.interfaces;
 
+import com.project.ecommerce.dtos.CategoryResponseDTO;
 import com.project.ecommerce.entity.Category;
 
 import java.util.List;
 
 public interface CategoryService {
-    List<Category> getAllCategories();
+    List<CategoryResponseDTO> getAllCategories();
     void createCategory(Category category);
     String deleteCategory(int categoryId);
     String updateCategory(Category category, int id);

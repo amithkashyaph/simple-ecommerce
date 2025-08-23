2bc728cbcb187384551f9eb7a66860939e142a60
amithkashyap <amithkashyaprv@gmail.com>
2025-08-23 23:17:33 +0530

    updated constants

--- src/main/java/com/project/ecommerce/config/AppConstants.java
+++ src/main/java/com/project/ecommerce/config/AppConstants.java
@@ -1,5 +1,8 @@
 package com.project.ecommerce.config;
 
 public class AppConstants {
-
+    public static final String DEFAULT_PAGE_NUMBER = "0";
+    public static final String DEFAULT_PAGE_SIZE = "50";
+    public static final String CATEGORY_SORT_BY = "categoryId";
+    public static final String SORT_ORDER = "asc";
 }

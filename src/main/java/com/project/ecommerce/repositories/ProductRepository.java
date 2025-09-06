c42ca4f3d4599e3f1b035c55402b2af0f6dd00a6
amithkashyap <amithkashyaprv@gmail.com>
2025-09-06 16:05:14 +0530

    updated Product repository interface

--- src/main/java/com/project/ecommerce/repositories/ProductRepository.java
+++ src/main/java/com/project/ecommerce/repositories/ProductRepository.java
@@ -1,4 +1,7 @@
 package com.project.ecommerce.repositories;
 
-public interface ProductRepository {
+import com.project.ecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }

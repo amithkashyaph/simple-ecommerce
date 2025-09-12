b6677b5329924aa42de867b353b03e355a272efe
amithkashyap <amithkashyaprv@gmail.com>
2025-09-12 22:26:14 +0530

    annotated product response dto class

--- src/main/java/com/project/ecommerce/dtos/ProductResponseDTO.java
+++ src/main/java/com/project/ecommerce/dtos/ProductResponseDTO.java
@@ -1,4 +1,11 @@
 package com.project.ecommerce.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductResponseDTO {
 }

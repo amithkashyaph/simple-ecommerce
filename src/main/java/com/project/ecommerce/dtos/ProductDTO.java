d11e16e4888f0034c96cba45ae33bbd1440b6eb3
amithkashyap <amithkashyaprv@gmail.com>
2025-09-12 22:25:15 +0530

    updated product dto class

--- src/main/java/com/project/ecommerce/dtos/ProductDTO.java
+++ src/main/java/com/project/ecommerce/dtos/ProductDTO.java
@@ -8,4 +8,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProductDTO {
+    private String productId;
+    private String name;
+    private String description;
+    private double price;
+    private double specialPrice;
 }

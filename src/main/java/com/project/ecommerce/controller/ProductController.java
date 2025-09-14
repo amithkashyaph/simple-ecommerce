31a42623dd3b19f732236fd87354ed0e808f20c8
amithkashyap <amithkashyaprv@gmail.com>
2025-09-14 23:25:04 +0530

    added initial GET API to fetch all products

--- src/main/java/com/project/ecommerce/controller/ProductController.java
+++ src/main/java/com/project/ecommerce/controller/ProductController.java
@@ -1,9 +1,18 @@
 package com.project.ecommerce.controller;
 
+import com.project.ecommerce.dtos.ProductResponseDTO;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/products")
 public class ProductController {
+
+    @GetMapping
+    public List<ProductResponseDTO> getAllProducts() {
+        return null;
+    }
 }

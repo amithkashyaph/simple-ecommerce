a689833466cc03f3e95d6e02924c299c01348ca5
amithkashyap <amithkashyaprv@gmail.com>
2025-09-15 23:07:25 +0530

    added getAllProducts in service interface

--- src/main/java/com/project/ecommerce/service/interfaces/ProductService.java
+++ src/main/java/com/project/ecommerce/service/interfaces/ProductService.java
@@ -1,4 +1,10 @@
 package com.project.ecommerce.service.interfaces;
 
+import com.project.ecommerce.dtos.ProductResponseDTO;
+
+import java.util.List;
+
 public interface ProductService {
+
+    List<ProductResponseDTO> getAllProducts();
 }

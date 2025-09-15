116c0b3014aea8c72676333c449d9464ff5b2817
amithkashyap <amithkashyaprv@gmail.com>
2025-09-15 23:07:53 +0530

    added initial getAllProducts service impl

--- src/main/java/com/project/ecommerce/service/ProductServiceImpl.java
+++ src/main/java/com/project/ecommerce/service/ProductServiceImpl.java
@@ -1,8 +1,15 @@
 package com.project.ecommerce.service;
 
+import com.project.ecommerce.dtos.ProductResponseDTO;
 import com.project.ecommerce.service.interfaces.ProductService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ProductServiceImpl implements ProductService {
+    @Override
+    public List<ProductResponseDTO> getAllProducts() {
+        return null;
+    }
 }

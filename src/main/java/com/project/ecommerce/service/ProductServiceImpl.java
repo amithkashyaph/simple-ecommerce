bb4317f8ceb2a46c0c84fde5638d3fd26769fad4
amithkashyap <amithkashyaprv@gmail.com>
2025-09-16 23:34:25 +0530

    wired product repository in service class

--- src/main/java/com/project/ecommerce/service/ProductServiceImpl.java
+++ src/main/java/com/project/ecommerce/service/ProductServiceImpl.java
@@ -1,13 +1,22 @@
 package com.project.ecommerce.service;
 
 import com.project.ecommerce.dtos.ProductResponseDTO;
+import com.project.ecommerce.repositories.ProductRepository;
 import com.project.ecommerce.service.interfaces.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ProductServiceImpl implements ProductService {
+
+    private ProductRepository productRepository;
+
+    @Autowired
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
     @Override
     public List<ProductResponseDTO> getAllProducts() {
         return null;

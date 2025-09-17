70ef42ae1ee98c60af8b9943aac825dac7def6f4
amithkashyap <amithkashyaprv@gmail.com>
2025-09-17 17:31:47 +0530

    wired product service in controller class

--- src/main/java/com/project/ecommerce/controller/ProductController.java
+++ src/main/java/com/project/ecommerce/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.project.ecommerce.controller;
 
 import com.project.ecommerce.dtos.ProductResponseDTO;
+import com.project.ecommerce.service.interfaces.ProductService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,11 @@ import java.util.List;
 @RequestMapping("/api/v1/products")
 public class ProductController {
 
+    private ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
     @GetMapping
     public List<ProductResponseDTO> getAllProducts() {
         return null;

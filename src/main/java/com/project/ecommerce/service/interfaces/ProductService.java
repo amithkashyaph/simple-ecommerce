c73dd77d63346e6bb810e600a67da5ce0cd87b1c
amithkashyap <amithkashyaprv@gmail.com>
2025-09-23 23:45:38 +0530

    updated product service interface to include add product API

--- src/main/java/com/project/ecommerce/service/interfaces/ProductService.java
+++ src/main/java/com/project/ecommerce/service/interfaces/ProductService.java
@@ -1,10 +1,13 @@
 package com.project.ecommerce.service.interfaces;
 
 import com.project.ecommerce.dtos.ProductResponseDTO;
+import com.project.ecommerce.entity.Product;
 
 import java.util.List;
 
 public interface ProductService {
 
     List<ProductResponseDTO> getAllProducts();
+
+    ProductResponseDTO addProduct(Product product);
 }
